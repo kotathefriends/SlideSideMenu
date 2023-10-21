90a4cdf79692653bd41923addec22ac801354d28
kotathefriends <5070kouta@gmail.com>
2023-10-21 17:18:18 +0900

    Initial Commit

---
+++ SlideSideMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideSideMenu
+//
+//  Created by KOTA TAKAHASHI on 2023/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideSideMenu/SlideSideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideSideMenuApp.swift
+//  SlideSideMenu
+//
+//  Created by KOTA TAKAHASHI on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SlideSideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
